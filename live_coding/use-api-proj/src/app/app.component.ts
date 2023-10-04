3bb6cbf563cfea2cdb2c47430decdfbe054928b8
onionmccabbage <onionmccabbage@live.com>
2023-10-04 16:55:40 +0100

    simple example using an Angular service

--- live_coding/use-api-proj/src/app/app.component.ts
+++ live_coding/use-api-proj/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TypicodeService } from './typicode.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'use-api-proj';
   // we need to revisit the 'any'
-  photos:Array<any> = []
+  photos: Array<any> = []
+  // we must instantiate our service in the constructor
+  constructor(private typicode: TypicodeService) {
+
+  }
+
+  // this method ALWAYS runs when this component is instantiated
+  ngOnInit() {
+    // make a call for the data
+    this.typicode.getAllPhotos().subscribe(
+      this.handlePhotos()
+    )
+  }
+  // todo: explain RxJS and Observables in Angular
+  handlePhotos() {
+    return (r:any)=>{
+      this.photos = r
+    }
+  }
+
 }
