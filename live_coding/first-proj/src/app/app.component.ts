c2547afad6faba2450ce3729a8129596168d9b95
onionmccabbage <onionmccabbage@live.com>
2023-10-04 11:07:48 +0100

    data binding examples

--- live_coding/first-proj/src/app/app.component.ts
+++ live_coding/first-proj/src/app/app.component.ts
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // properties of this component
   title = 'Coffee Break';
-  drink = 'Latte'
+  drink = 'Latte';
+  i = 'https://placekitten.com/120/64';
+  a = 'random kitten';
+  t = 'now thats clever';
+
+  stocks = ['AAPL', 'AMAZN', 'C', 'FB', 'TSLA']
+  portfolio = [
+    {'ticker':'AAPL', 'qty':10, 'price':100},
+    {'ticker':'AMAZN', 'qty':20, 'price':200},
+    {'ticker':'C', 'qty':30, 'price':300},
+    {'ticker':'FB', 'qty':40, 'price':400},
+    {'ticker':'TSLA', 'qty':50, 'price':500}
+  ]
+
+
+
+  // methids of this component
+  handleStuff(e:any){ // typescript needs this
+    console.log(`something happened ${e}`)
+  }
+
 }
