560feeb81c4d71fb096b26a143ede1b7dbcbdaf4
onionmccabbage <onionmccabbage@live.com>
2023-10-05 16:06:31 +0100

    some routing components

--- live_coding/routed-proj/src/app/app.module.ts
+++ live_coding/routed-proj/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { AboutComponent } from './about/about.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    AboutComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
